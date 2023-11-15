c900bafa091209978c3027447cbcd21497373b6c
Lucas Kenzo Awane de Souza <kenzoawane@gmail.com>
2023-11-15 00:04:42 -0300

    Insercao do usuario com POST

--- src/main/java/com/kenzo/workshopmongo/user/controller/UserController.java
+++ src/main/java/com/kenzo/workshopmongo/user/controller/UserController.java
@@ -3,12 +3,14 @@ package com.kenzo.workshopmongo.user.controller;
 import com.kenzo.workshopmongo.user.dto.UserDTO;
 import com.kenzo.workshopmongo.user.model.User;
 import com.kenzo.workshopmongo.user.service.UserService;
+import org.apache.logging.log4j.util.PerformanceSensitive;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,5 +28,18 @@ public class UserController {
         var users = service.findAll();
         var usersDto = users.stream().map(UserDTO::new).toList();
         return ResponseEntity.ok().body(usersDto);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        var user = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(user));
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> insert(@RequestBody User obj){
+        var user  = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/kenzo/workshopmongo/user/service/UserService.java
+++ src/main/java/com/kenzo/workshopmongo/user/service/UserService.java
@@ -1,11 +1,13 @@
 package com.kenzo.workshopmongo.user.service;
 
+import com.kenzo.workshopmongo.comum.exception.service.ObjectNotFoundException;
 import com.kenzo.workshopmongo.user.model.User;
 import com.kenzo.workshopmongo.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,4 +18,14 @@ public class UserService {
     public List<User> findAll(){
         return repository.findAll();
     }
+
+    public User findById(String id){
+        Optional<User> user =  repository.findById(id);
+        return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
 }
